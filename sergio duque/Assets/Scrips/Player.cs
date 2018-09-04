55799758da0aa52e6bd2fc8d24a9a953c1b60a7a
cheitovilla <cheitovilla@outlook.com>
2018-09-03 20:42:07 -0500

    Base

---
+++ sergio duque/Assets/Scrips/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    CharacterController controller;
+
+
+
+
+    // Use this for initialization
+    private void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movimiento();
+
+    }
+    public void Movimiento()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
