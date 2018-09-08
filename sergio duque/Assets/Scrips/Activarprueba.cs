10ba14f9fa31f6320f1efa5b1339d529d5bbb3ac
DesmondCF <Canserbero201585@gmail.com>
2018-09-08 12:24:37 -0500

    sensores

---
+++ sergio duque/Assets/Scrips/Activarprueba.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activarprueba : MonoBehaviour {
+
+    public GameObject prueba;
+
+    private void Start() {
+
+        prueba.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            prueba.SetActive(true);
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other) {
+
+        if (other.transform.tag == "Player")
+        { 
+            prueba.SetActive(false);
+        }
+    }
+}
