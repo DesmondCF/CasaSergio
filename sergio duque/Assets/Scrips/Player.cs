1394fe18545b96ef609c2f848e97780d2ff3c1f9
DesmondCF <Canserbero201585@gmail.com>
2018-09-15 12:58:12 -0500

    se modifico prueba

--- sergio duque/Assets/Scrips/Player.cs
+++ sergio duque/Assets/Scrips/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
     public float rotateSpeed = 3.0F;
     CharacterController controller;
 
+    public GameObject cam;
+    public float rot_x;
 
 
 
@@ -32,9 +34,33 @@ public class Player : MonoBehaviour
     }
     public void Movimiento()
     {
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
+        //cam.transform.Rotate(Input.GetAxis("Mouse Y")*-1,0,0);
+        rot_x += (Input.GetAxis("Mouse Y") * -1) * rotateSpeed;
+        
+        if (rot_x > 0 && rot_x < 60)
+        {
+            cam.transform.localRotation = Quaternion.Euler(rot_x, 0, 0);
+        }
+            
+        if(rot_x < 0 && rot_x > -60)
+        {
+            cam.transform.localRotation = Quaternion.Euler(rot_x, 0, 0);
+        }
+        if(rot_x > 60)
+        {
+            rot_x = 60;
+        }
+        if ( rot_x < -60)
+        {
+            rot_x  = - 60;
+        }
+
+
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
     }
 }
---
+++ sergio duque/Assets/Scrips/ShotSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotSystem : MonoBehaviour {
+
+    public GameObject pref_Bullet;
+    GameObject bullet;
+
+    public Transform spawnPoint;
+    public float force;
+
+    // Use this for initialization
+    private void Update() { 
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+	}
+	
+	// Update is called once per frame
+	void Shot () {
+        bullet = Instantiate(pref_Bullet, spawnPoint.position,Quaternion.identity);
+        bullet.AddComponent<Rigidbody>();
+        bullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward *force, ForceMode.Impulse);
+        Destroy(bullet, 5f);
+		
+	}
+}
